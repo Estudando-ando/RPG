363efda7ddf110f9d613e7b2cedcfe86b87266cd
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-08-31 15:24:26 -0300

    Criando personagem abstrato e guerreiro

---
+++ Guerreiro.java
@@ -0,0 +1,6 @@
+public class Guerreiro extends Personagem {
+
+  public Guerreiro(String nome, int vida) {
+    super(nome, vida);
+  }
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Guerreiro personagem = new Guerreiro("Gustavo", 80);
+
+    System.out.println(personagem);
+  }
+
+  /*
+   * criar branch 
+   *  git checkout -b nome-branch
+   * 
+   *  especificar o que vai ser enviado ao git
+   *  git add <especificar-arquivo> / git add . 
+   * 
+   *  adicionar mensagem de commit
+   *  git commit -m "Mensagem"
+   * 
+   *  enviar ao github
+   *  git push origin nome-branch
+   */
+
+}
